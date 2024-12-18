0cdaa1cac42b1ebd27c26b3251a2a5080e461944
Tom Nielsen <tanielsen@gmail.com>
2024-12-18 11:53:40 +0000

    basically working

--- select_and_run_action.js
+++ select_and_run_action.js
@@ -21,13 +21,17 @@ const {
   eval_expression,
 } = require("@saltcorn/data/models/expression");
 const { getState } = require("@saltcorn/data/db/state");
+const { interpolate } = require("@saltcorn/data/utils");
 
 const db = require("@saltcorn/data/db");
 const {
   stateFieldsToWhere,
   picked_fields_to_query,
 } = require("@saltcorn/data/plugin-helper");
-const { getActionConfigFields } = require("@saltcorn/data/plugin-helper");
+const {
+  getActionConfigFields,
+  run_action_column,
+} = require("@saltcorn/data/plugin-helper");
 
 const configuration_workflow = () =>
   new Workflow({
@@ -41,7 +45,7 @@ const configuration_workflow = () =>
           const stateActions = Object.entries(getState().actions).filter(
             ([k, v]) => !v.disableInList
           );
-          
+
           const stateActionKeys = stateActions.map(([k, v]) => k);
 
           const triggerActions = Trigger.trigger_actions({
@@ -55,15 +59,14 @@ const configuration_workflow = () =>
           const actionConfigFields = [];
 
           for (const [name, action] of stateActions) {
-            
             if (!stateActionKeys.includes(name)) continue;
             const cfgFields = await getActionConfigFields(action, table);
-            
+
             for (const field of cfgFields) {
               const cfgFld = {
                 ...field,
                 showIf: {
-                  action_name: name,                 
+                  action_name: name,
                   ...(field.showIf || {}),
                 },
               };
@@ -71,8 +74,7 @@ const configuration_workflow = () =>
               actionConfigFields.push(cfgFld);
             }
           }
-          console.log(actionConfigFields);
-          
+
           return new Form({
             fields: [
               {
@@ -101,168 +103,61 @@ const get_state_fields = async (table_id, viewname, { columns }) => [];
 const run = async (
   table_id,
   viewname,
-  { relation, maxHeight, where, disabled, ajax, stay_open_on_select },
+  { label, action_name },
   state,
   extra
 ) => {
-  const { id } = state;
-  if (!id) return "need id";
   const req = extra.req;
 
-  if (!relation) {
-    throw new Error(
-      `Select2 many-to-many view ${viewname} incorrectly configured. No relation chosen`
-    );
-  }
-  const relSplit = relation.split(".");
-  if (relSplit.length < 4) {
-    throw new Error(
-      `Select2 many-to-many view ${viewname} incorrectly configured. No relation chosen`
-    );
-  }
   const rndid = `bs${Math.round(Math.random() * 100000)}`;
-  const [relTableNm, relField, joinFieldNm, valField] = relSplit;
+
   const table = await Table.findOne({ id: table_id });
 
-  const relTable = await Table.findOne({ name: relTableNm });
-  await relTable.getFields();
-  const joinField = relTable.fields.find((f) => f.name === joinFieldNm);
-  const joinedTable = await Table.findOne({ name: joinField.reftable_name });
   const rows = await table.getJoinedRows({
-    where: { id },
+    where: {},
     forPublic: !req.user || req.user.role_id === 100, // TODO in mobile set user null for public
     forUser: req.user,
-    aggregations: {
-      _selected: {
-        table: joinField.reftable_name,
-        ref: "id",
-        subselect: {
-          field: joinFieldNm,
-          table: { name: db.sqlsanitize(relTable.name) }, //legacy, workaround insufficient escape
-          whereField: relField,
-        },
-        field: valField,
-        aggregate: "ARRAY_AGG",
-      },
-    },
   });
   if (!rows[0]) return "No row selected";
-  let possibles = [];
-  if (!ajax) {
-    possibles = await joinedTable.distinctValues(
-      valField,
-      where
-        ? jsexprToWhere(
-            where,
-            { ...rows[0], user: req.user },
-            joinedTable.getFields()
-          )
-        : undefined
-    );
-  } else {
-    possibles = rows[0]._selected || [];
-  }
-  possibles.sort((a, b) => {
-    const fa = a?.toLowerCase?.();
-    const fb = b?.toLowerCase?.();
-    return fa > fb ? 1 : fb > fa ? -1 : 0;
-  });
-  const selected = new Set(rows[0]._selected || []);
+
   return (
     select(
-      { id: rndid, multiple: "multiple", class: "no-form-change" },
-      possibles.map((p) => option({ selected: selected.has(p) }, p))
+      { id: rndid },
+      rows.map((row) =>
+        option({ value: row[table.pk_name] }, interpolate(label, row, req.user))
+      )
     ) +
     script(
       domReady(
         `$('#${rndid}').select2({ 
             width: '100%', 
-            ${disabled ? "disabled: true," : ""}
-            ${stay_open_on_select ? "closeOnSelect: false," : ""}
             dropdownParent: $('#${rndid}').parent(), 
             dropdownCssClass: "select2-dd-${rndid}",
-            ${
-              ajax
-                ? ` minimumInputLength: 2,
-            minimumResultsForSearch: 10,
-            ajax: {
-                url: "/api/${joinedTable.name}",
-                dataType: "json",
-                type: "GET",
-                data: function (params) {
-        
-                    var queryParameters = {
-                        ${valField}: params.term,
-                        approximate: true
-                    }
-                    return queryParameters;
-                },
-                processResults: function (data) {
-                    if(!data || !data.success) return [];
-                    return {
-                        results: $.map(data.success, function (item) {
-                            return {
-                                text: item.${valField},
-                                id: item.${valField}
-                            }
-                        })
-                    };
-                  }},`
-                : ""
-            }
+
         });
-        $('#${rndid}').on('select2:unselect', function (e) {
-            view_post('${viewname}', 'remove', {id:'${id}', value: e.params.data.id});
-        });
+       
         $('#${rndid}').on('select2:select', function (e) {
-            view_post('${viewname}', 'add', {id:'${id}', value: e.params.data.id});
+            view_post('${viewname}', 'go', {id: e.params.data.id, state: ${JSON.stringify(
+          state
+        )}});
         });`
       )
-    ) +
-    (maxHeight
-      ? style(
-          `.select2-container--default .select2-dd-${rndid} .select2-results>.select2-results__options {max-height: ${maxHeight}px;}`
-        )
-      : "")
+    )
   );
 };
 
-const go = async (
-  table_id,
-  viewname,
-  { relation, field_values_formula },
-  { id },
-  { req }
-) => {
+const go = async (table_id, viewname, cfg, { id, state }, { req }) => {
   const table = await Table.findOne({ id: table_id });
-  const rows = await table.getJoinedRows({
-    where: { id },
-    forPublic: !req.user || req.user.role_id === 100, // TODO in mobile set user null for public
-    forUser: req.user,
+  const row = await table.getRow({ [table.pk_name]: id });
+  if (!row) return { json: { error: "Row not found" } };
+  const col = { ...cfg, configuration: cfg };
+  const result = await run_action_column({
+    col,
+    req,
+    table,
+    row: { ...row, state },
   });
-  if (!rows[0]) return { json: { error: "Row not found" } };
-  let extra = {};
-  if (field_values_formula) {
-    extra = eval_expression(field_values_formula, rows[0], req.user);
-  }
-  const relSplit = relation.split(".");
-  const [joinTableNm, relField, joinFieldNm, valField] = relSplit;
-  const joinTable = await Table.findOne({ name: joinTableNm });
-  await joinTable.getFields();
-  const joinField = joinTable.fields.find((f) => f.name === joinFieldNm);
-  const joinedTable = await Table.findOne({ name: joinField.reftable_name });
-  const joinedRow = await joinedTable.getRow({ [valField]: value });
-  const result = {};
-  await joinTable.insertRow(
-    {
-      [relField]: id,
-      [joinFieldNm]: joinedRow.id,
-      ...extra,
-    },
-    req.user || { role_id: 100 },
-    result
-  );
-  return { json: { success: "ok", ...result } };
+  return { json: { success: "ok", ...(result || {}) } };
 };
 
 module.exports = {
